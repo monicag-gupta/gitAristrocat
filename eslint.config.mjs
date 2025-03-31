aff055638e184e1f4529c0b263fe55f9f54c2be3
Monica Gupta <gupta.monicag@gmail.com>
2025-03-31 19:54:34 +0530

    Testing Husky

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+// import { defineConfig } from "eslint/config";
+// import globals from "globals";
+// import js from "@eslint/js";
+
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs}"] },
+//   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+// ]);
+
+
+
+
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js"; 
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { 
+      globals: globals.browser 
+    },
+    rules: {
+      //"no-console": "error",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"]
+    },
+    extends: [js.configs.recommended] // Correct way to extend rules
+  }
+]);
